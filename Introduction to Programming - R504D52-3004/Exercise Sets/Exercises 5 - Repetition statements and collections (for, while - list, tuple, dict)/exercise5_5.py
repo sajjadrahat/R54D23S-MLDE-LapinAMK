6720e8c8e47438bf5df5aca214b39cc0ae7131e2
Rahat-S-M <150609477+Rahat-S-M@users.noreply.github.com>
2023-11-12 23:03:08 +0200

    New exercise

---
+++ Introduction to Programming - R504D52-3004/Exercise Sets/Exercises 5 - Repetition statements and collections (for, while - list, tuple, dict)/exercise5_5.py
@@ -0,0 +1,3 @@
+months_tuple = ("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December")
+user_number = int(input('Give the number of month: \n'))
+print(months_tuple[user_number-1])

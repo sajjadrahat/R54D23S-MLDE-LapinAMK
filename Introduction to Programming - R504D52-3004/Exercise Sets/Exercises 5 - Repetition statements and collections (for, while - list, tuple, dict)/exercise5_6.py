0e988e9b715b28903089748209cee98a52808c88
Rahat-S-M <150609477+Rahat-S-M@users.noreply.github.com>
2023-11-12 23:09:00 +0200

    Exercise 5 Added

---
+++ Introduction to Programming - R504D52-3004/Exercise Sets/Exercises 5 - Repetition statements and collections (for, while - list, tuple, dict)/exercise5_6.py
@@ -0,0 +1,6 @@
+dict1 = {"name": "Pulp Fiction", "year": 1994, "director": "Quentin Tarantino", "genre": "Crime, Drama", "duration": 154}
+print(dict1["name"])
+print(dict1["year"])
+print(dict1["director"])
+print(dict1["genre"])
+print(dict1["duration"])
